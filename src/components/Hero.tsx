e938633ac3b0351254fab13fe6dbcd49248efa7b
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-04 15:51:57 +0200

    work on hero img

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Hero() {
     return (
-        <div className="w-72">
-            <img src="/kahp.jpg" alt="" />
+        <div className="w-72 shadow-[0_-35px_30px_-15px_red] ">
+            <img src="/kahp.jpg" alt="" className="w-72" />
         </div>
     );
 }
