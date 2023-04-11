3afb66dffeb638445d8a759752f2417617bd6bb2
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-11 19:31:55 +0200

    removed navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
 import MainPage from "@/components/MainPage";
 
 export default function index() {
     return (
         <div>
-            <Navbar />
             <MainPage />
         </div>
     );
