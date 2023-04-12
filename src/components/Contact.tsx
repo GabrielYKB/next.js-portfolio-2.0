be2be1ab94eb9d155878a7716f4e5c1f6fba38f2
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-12 16:54:54 +0200

    started work on contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Contact() {
-    return <div className="h-96">Contact</div>;
+    return (
+        <div className="flex justify-around h-96">
+            <div>
+                <p>Contact information:</p>
+                <p>Gabriel.yakooub@gmail.com</p>
+                <p>070-070 11 17</p>
+            </div>
+            <div>Location</div>
+        </div>
+    );
 }
