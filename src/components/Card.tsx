2816621fbdda8e6eeaf9a04d1793cf4ee4b26f7e
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-19 23:00:41 +0200

    update on cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,23 +5,27 @@ export default function Card() {
     const cardsData = [
         {
             label: "img1",
-            content: "description",
-            link: "",
+            content: "Kanban Board",
+            link: "https://github.com/GabrielYKB/kanban-board",
+            demo: "https://chimera-ant.netlify.app/",
         },
         {
             label: "img2",
             content: "description2",
             link: "",
+            demo: "",
         },
         {
             label: "img3",
             content: "description3",
             link: "",
+            demo: "",
         },
         {
             label: "img4",
             content: "description4",
             link: "",
+            demo: "",
         },
     ];
 
@@ -35,8 +39,13 @@ export default function Card() {
                         <div className="h-80 w-56 bg-violet-500 flex rounded-lg items-center justify-start flex-col">
                             <div className=" bg-red-500 w-52 h-48 rounded-lg m-2 ">
                                 <div className="h-8 w-8 bg-white fixed right-1 top-1 rounded-full">
-                                    <a href="">
+                                    <a href={card.link}>
                                         <img src="github-mark.svg" />
+                                    </a>
+                                </div>
+                                <div className="h-8 w-8 bg-white fixed left-1 top-1 rounded-full">
+                                    <a href={card.demo}>
+                                        <img src="" />
                                     </a>
                                 </div>
                                 {card.label}
