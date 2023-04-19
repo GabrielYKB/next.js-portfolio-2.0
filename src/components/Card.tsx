b3361cf411f59c01f7a5b0a515dd6d7b1eab1d96
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-19 14:55:52 +0200

    checkpoint

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,15 +2,34 @@ import React from "react";
 import Tilt from "react-parallax-tilt";
 
 export default function Card() {
+    const cardsData = [
+        {
+            label: "img1",
+            content: "description",
+        },
+        {
+            label: "img2",
+            content: "description2",
+        },
+        {
+            label: "img3",
+            content: "description3",
+        },
+        {
+            label: "img4",
+            content: "description4",
+        },
+    ];
+
     return (
         <div className="">
             <Tilt>
                 <div className="h-80 w-56 bg-violet-500 flex rounded-lg items-center justify-start flex-col">
-                    <div className=" bg-red-500 w-48 h-48 m-5 rounded-lg">
-                        img
+                    <div className=" bg-red-500 w-52 h-48 rounded-lg m-2">
+                        {cardsData.label}
                     </div>
-                    <div className=" bg-orange-500 w-48 h-16 rounded-lg">
-                        desc
+                    <div className=" bg-orange-500 w-52 h-24 mt-2 rounded-lg">
+                        {cardsData.map}
                     </div>
                 </div>
             </Tilt>
