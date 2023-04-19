23bcfc444fa2c653580ec05c70f0ed883af957a4
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-19 14:36:58 +0200

    card template

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,14 @@ export default function Card() {
     return (
         <div className="">
             <Tilt>
-                <div className="h-80 w-56 bg-blue-400"></div>
+                <div className="h-80 w-56 bg-violet-500 flex rounded-lg items-center justify-start flex-col">
+                    <div className=" bg-red-500 w-48 h-48 m-5 rounded-lg">
+                        img
+                    </div>
+                    <div className=" bg-orange-500 w-48 h-16 rounded-lg">
+                        desc
+                    </div>
+                </div>
             </Tilt>
         </div>
     );
