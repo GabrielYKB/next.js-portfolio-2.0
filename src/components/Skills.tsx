d68a752aa9f693b4b5b8be523e36deab51ecee69
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-21 15:49:09 +0200

    code improvment to skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -38,32 +38,18 @@ export default function Skills() {
 
     return (
         <div className="flex justify-evenly h-96">
-            <div className="flex flex-col items-center">
-                <div className="border-b-2 border-[var(--detail)] w-64 flex pb-1 justify-center mb-2">
-                    <h3>Frontend</h3>
-                </div>
-                <p>React</p>
-                <p>Javascript</p>
-                <p>Typescrit</p>
-                <p>Tailwind CSS</p>
-                <p>CSS</p>
-            </div>
-            <div className="flex flex-col items-center">
-                <div className="border-b-2 border-[var(--detail)] w-64 flex justify-center pb-1 mb-2">
-                    <h3>Backend</h3>
-                </div>
-                <p>Node.JS</p>
-                <p>Express</p>
-                <p>MongoDB</p>
-                <p>API</p>
-            </div>
-            <div className="flex flex-col items-center">
-                <div className="border-b-2 border-[var(--detail)] w-64 flex justify-center mb-2 pb-1">
-                    <h3>Other</h3>
-                </div>
-                <p>Git/Github</p>
-                <p>NPM</p>
-            </div>
+            {skillsData.map((skill, dsc) => {
+                return (
+                    <div className="flex flex-col items-center text-center">
+                        <div>
+                            <div className="border-b-2 border-[var(--detail)] w-64 flex pb-1 justify-center mb-2">
+                                <h3>{skill.headline}</h3>
+                            </div>
+                            {skill.content}
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 }
