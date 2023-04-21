450dde6c259c23f0f8dec25c8ea4a0517a07f397
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-21 15:41:07 +0200

    remap test

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,41 @@
 import React from "react";
 
 export default function Skills() {
+    const skillsData = [
+        {
+            headline: "Frontend",
+            content: (
+                <div>
+                    <p>React</p>
+                    <p>Javascript</p>
+                    <p>Typescrit</p>
+                    <p>Tailwind CSS</p>
+                    <p>CSS</p>
+                </div>
+            ),
+        },
+        {
+            headline: "Backend",
+            content: (
+                <div>
+                    <p>Node.JS</p>
+                    <p>Express</p>
+                    <p>MongoDB</p>
+                    <p>API</p>
+                </div>
+            ),
+        },
+        {
+            headline: "Other",
+            content: (
+                <div>
+                    <p>Git/Github</p>
+                    <p>NPM</p>
+                </div>
+            ),
+        },
+    ];
+
     return (
         <div className="flex justify-evenly h-96">
             <div className="flex flex-col items-center">
