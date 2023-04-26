383f3d00d33d8d5ace26afd472cc637295718147
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-26 17:21:03 +0200

    update on contacts

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,6 @@ export default function About() {
     return (
         <div className="h-96 flex justify-between px-56">
             <div>About me</div>
-            <div>Links</div>
         </div>
     );
 }
