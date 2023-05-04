eae3d994d5e20ee10a5e84de78ede6a709c37076
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-05-04 18:11:49 +0200

    quick fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,21 +4,25 @@ export default function About() {
     return (
         <div className="h-96 ">
             <div className=" flex justify-between bg-[var(--secondary)] rounded-lg text-center pt-3 text-lg p-4 font-light text-gray-200">
-                As a junior software developer with a diverse set of skills
-                ranging from frontend technologies like React, Javascript,
-                Typescript, and Tailwind CSS to backend technologies like
-                Node.JS, Express, and MongoDB, I have all the tools necessary to
-                build robust and scalable applications. However, what sets me
-                apart from others is my passion for coding and love for problem
-                solving. I thrive on tackling complex problems and finding
-                elegant solutions that not only meet the requirements but also
-                exceed the expectations of my clients and end-users. My love for
-                coding goes beyond just the technical aspects of it. For me,
-                coding is a form of self-expression, a way to turn my ideas into
-                reality, and a means to create something meaningful that has a
-                positive impact on the world. I take pride in my work and I'm
-                constantly pushing myself to learn and grow, staying up-to-date
-                with the latest trends and best practices in the industry.
+                <p>
+                    As a junior software developer with a diverse set of skills
+                    ranging from frontend technologies like React, Javascript,
+                    Typescript, and Tailwind CSS to backend technologies like
+                    Node.JS, Express, and MongoDB, I have all the tools
+                    necessary to build robust and scalable applications.
+                    However, what sets me apart from others is my passion for
+                    coding and love for problem solving. I thrive on tackling
+                    complex problems and finding elegant solutions that not only
+                    meet the requirements but also exceed the expectations of my
+                    clients and end-users. My love for coding goes beyond just
+                    the technical aspects of it. For me, coding is a form of
+                    self-expression, a way to turn my ideas into reality, and a
+                    means to create something meaningful that has a positive
+                    impact on the world. I take pride in my work and I'm
+                    constantly pushing myself to learn and grow, staying
+                    up-to-date with the latest trends and best practices in the
+                    industry.
+                </p>
             </div>
         </div>
     );
