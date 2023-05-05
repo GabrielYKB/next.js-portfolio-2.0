c7627462968d3ca8c84706be94cea7c68e641354
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-05-05 17:57:13 +0200

    more work on work sites

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,36 +3,38 @@ import Typewriter from "typewriter-effect";
 
 export default function Intro() {
     return (
-        <div
-            id="home"
-            className="p-6 bg-[var(--secondary)] rounded-3xl h-screen"
-            style={{ width: 700 }}
-        >
-            <div className="font-black text-transparent text-4xl bg-clip-text bg-gradient-to-l from-[var(--detail)] to-white max-w-xl">
-                Hello!
-            </div>
-            <div className="text-white text-2xl font-medium">
-                <div className="py-4">
+        <div className="h-screen">
+            <div
+                id="home"
+                className="p-6 pb-10 bg-[var(--secondary)] rounded-3xl"
+                style={{ width: 1000 }}
+            >
+                <div className="font-black text-transparent text-7xl bg-clip-text bg-gradient-to-l from-[var(--detail)] to-white max-w-xl">
+                    Hello!
+                </div>
+                <div className="text-white text-4xl font-medium">
+                    <div className="py-4">
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .changeDelay(55)
+                                    .typeString(
+                                        "My name is Gabriel and I'm a Junior software developer. "
+                                    )
+                                    .start();
+                            }}
+                        />
+                    </div>
                     <Typewriter
                         onInit={(typewriter) => {
                             typewriter
                                 .changeDelay(55)
-                                .typeString(
-                                    "My name is Gabriel and I'm a Junior software developer. "
-                                )
+                                .pauseFor(4000)
+                                .typeString("Welcome to my portfolio page!")
                                 .start();
                         }}
                     />
                 </div>
-                <Typewriter
-                    onInit={(typewriter) => {
-                        typewriter
-                            .changeDelay(55)
-                            .pauseFor(4000)
-                            .typeString("Welcome to my portfolio page!")
-                            .start();
-                    }}
-                />
             </div>
         </div>
     );
