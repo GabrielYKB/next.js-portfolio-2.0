4d7bbe112735abc802aad8ef9703108716ce2871
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-05-07 16:31:31 +0200

    work on scroll down indicator done

--- src/components/ScrollDownIndicator.tsx
+++ src/components/ScrollDownIndicator.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-scroll";
 import React, { useEffect, useState } from "react";
 
 const ScrollDownIndicator: React.FC = () => {
@@ -8,7 +9,7 @@ const ScrollDownIndicator: React.FC = () => {
             const currentScrollPosition = window.pageYOffset;
             const screenHeight = window.innerHeight;
 
-            if (currentScrollPosition > screenHeight * 0.3) {
+            if (currentScrollPosition > screenHeight * 0.23) {
                 setIsVisible(false);
             } else {
                 setIsVisible(true);
@@ -28,18 +29,26 @@ const ScrollDownIndicator: React.FC = () => {
                 isVisible ? "opacity-100" : "opacity-0"
             }`}
         >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-gray-500 animate-bounce"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+            <Link
+                to="about"
+                smooth={true}
+                duration={500}
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                offset={-100}
             >
-                <path
-                    fillRule="evenodd"
-                    d="M10 14a1 1 0 01-.71-.29l-3.5-3.5a1 1 0 011.42-1.42L10 11.59l3.29-3.3a1 1 0 011.42 1.42l-3.5 3.5A1 1 0 0110 14z"
-                    clipRule="evenodd"
-                />
-            </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-10 text-gray-500 animate-bounce cursor-pointer hover:bg-gray-300 rounded-full duration-300 hover:duration-300 hover:bg-opacity-20"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M10 14a1 1 0 01-.71-.29l-3.5-3.5a1 1 0 011.42-1.42L10 11.59l3.29-3.3a1 1 0 011.42 1.42l-3.5 3.5A1 1 0 0110 14z"
+                        clipRule="evenodd"
+                    />
+                </svg>{" "}
+            </Link>
         </div>
     );
 };
