4aedca8b38e3d1b691f9f42c315f451b4c96f2f3
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-05-11 17:57:38 +0200

    fixes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,44 +8,40 @@ export default function Navbar() {
                     to="home"
                     smooth={true}
                     duration={500}
-                    className="text-gray-300 hover:text-white rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:text-white rounded-md text-sm font-medium cursor-pointer"
                     offset={-300}
                 >
                     Home
                 </Link>
             </div>
-            <nav className="flex  ">
-                <div className="flex gap-6">
-                    <Link
-                        to="about"
-                        smooth={true}
-                        duration={500}
-                        className="text-gray-300 hover:text-white rounded-md text-sm font-medium"
-                        offset={-100}
-                    >
-                        About
-                    </Link>
-
-                    <Link
-                        to="cards"
-                        smooth={true}
-                        duration={500}
-                        className="text-gray-300 hover:text-white rounded-md text-sm font-medium"
-                        offset={-120}
-                    >
-                        Work
-                    </Link>
-
-                    <Link
-                        to="contact"
-                        smooth={true}
-                        duration={500}
-                        className="text-gray-300 hover:text-white  rounded-md text-sm font-medium"
-                        offset={-200}
-                    >
-                        Contact
-                    </Link>
-                </div>
+            <nav className="flex gap-6">
+                <Link
+                    to="about"
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-300 hover:text-white rounded-md text-sm font-medium cursor-pointer"
+                    offset={-100}
+                >
+                    About
+                </Link>
+                <Link
+                    to="cards"
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-300 hover:text-white rounded-md text-sm font-medium cursor-pointer"
+                    offset={-120}
+                >
+                    Work
+                </Link>
+                <Link
+                    to="contact"
+                    smooth={true}
+                    duration={500}
+                    className="text-gray-300 hover:text-white  rounded-md text-sm font-medium cursor-pointer"
+                    offset={-200}
+                >
+                    Contact
+                </Link>
             </nav>
         </div>
     );
